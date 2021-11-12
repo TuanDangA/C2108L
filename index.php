69ac46f3f88ff3720cd7b4f9e12b63b83cf15a66
TuanDangA <tuananhdangktqd@gmail.com>
2021-11-12 20:30:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,14 @@
 </head>
 <body>
     <p>list</p>
-    <ol>
-        <li><a href="./1571/Unesco & Gallery/unesco.html" target="blank">unesco.html</a>
-    </ol>
+    <ul>
+        <li><a href="./1571/Unesco & Gallery/unesco.html" target="blank" list-style-type="circle">unesco.html</a>
+        <li><a href="./1571/Unesco & Gallery/GalleryOne.html" target="blank" list-style-type="circle">GalleryOne.html</a>
+        <li><a href="./1571/Unesco & Gallery/GalleryTwo.html" target="blank" list-style-type="circle">GalleryTwo.html</a>
+        <li><a href="./1571/Vincent State School/home.html" target="blank" list-style-type="circle">home.html</a>
+        <li><a href="./1571/Vincent State School/admission.html" target="blank" list-style-type="circle">admission.html</a>
+        <li><a href="./1571/Vincent State Schooly/fee.html" target="blank" list-style-type="circle">fee.html</a>
+        <li><a href="./1571/Vincent State School/information.html" target="blank" list-style-type="circle">information.html</a>
+    </ul>
 </body>
 </html>
