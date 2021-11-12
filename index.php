6824d4ac5bb6c595bd25c98002396bcb505b050a
TuanDangA <tuananhdangktqd@gmail.com>
2021-11-12 20:21:36 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework_1571</title>
+</head>
+<body>
+    <p>list</p>
+    <ol>
+        <li><a href="./1571/Unesco & Gallery/unesco.html" target="blank">unesco.html</a>
+    </ol>
+</body>
+</html>
