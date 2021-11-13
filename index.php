248e7bf6c761863c48863c30b18e5ef222b9440e
TuanDangA <tuananhdangktqd@gmail.com>
2021-11-13 16:51:56 +0700

    Update 1553, 1554

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homework_1571</title>
+    <title>Homework</title>
 </head>
 <body>
-    <p>list</p>
+    <p>1571</p>
     <ul>
         <li><a href="./1571/Unesco & Gallery/unesco.html" target="blank" list-style-type="circle">unesco.html</a>
         <li><a href="./1571/Unesco & Gallery/GalleryOne.html" target="blank" list-style-type="circle">GalleryOne.html</a>
@@ -16,6 +16,19 @@
         <li><a href="./1571/Vincent State School/admission.html" target="blank" list-style-type="circle">admission.html</a>
         <li><a href="./1571/Vincent State Schooly/fee.html" target="blank" list-style-type="circle">fee.html</a>
         <li><a href="./1571/Vincent State School/information.html" target="blank" list-style-type="circle">information.html</a>
+    </ul>
+    <br>
+    <p>1553</p>
+    <ul>
+        <li>Basic</li>
+        <li><a href="./1553/Grammar.html" target="blank">Grammar.html</a></li>
+        <li><a href="./1553/style.css" target="blank">style.css</a></li>
+        <li>Advanced</li>
+        <li><a href="./1554/TouristAttractions.html" target="blank">TouristAttractions.html</a></li>
+        <li><a href="./1554/London.html" target="blank">London.html</a></li>
+        <li><a href="./1554/Paris.html" target="blank">Paris.html</a></li>
+        <li><a href="./1554/Italy.html" target="blank">Italy.css</a></li>
+        <li><a href="./1554/style_2.css" target="blank">style_2.css</a></li>
     </ul>
 </body>
 </html>
